a9a10b278eebf252dfa007a021dbda7955a285d3
vikasrath <vikasrathote01322@gmail.com>
2024-11-06 09:44:05 -0800

    ....

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{headerShown:true}}/>
+      <Stack.Screen name="index"  options={{headerShown:true,title:"Career Sathi"}}/>
     </Stack>
   );
 }
